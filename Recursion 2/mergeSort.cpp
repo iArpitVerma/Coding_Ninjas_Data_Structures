af814d03b06f7957b96f1277284d075addbbf764
Arpit Verma <arpitverma568@gmail.com>
2022-09-01 19:59:49 +0530

    5th Day

--- Recursion 2/mergeSort.cpp
+++ Recursion 2/mergeSort.cpp
@@ -1,42 +1,38 @@
 #include <iostream>
 using namespace std;
-void mergeTwoSortedArrays(int arr[], int si,int ei){
-    int mid = (si+ei)/2;
-    int i =si , j=mid+1 , index =0 ,l =0;
-    int output[ei-si+1];
+void merge(int arr[],int si, int mid, int ei){
+    int temp[ei-si+1];
+    int i = si, j = mid+1, k=0;
     while(i<=mid && j<=ei){
-        if(arr[i]<=arr[j]){
-            output[index] = arr[i];
-            i++, index++ ; 
+        if(arr[i] < arr[j]){
+            temp[k++] = arr[i++];
+        }else{
+            temp[k++] = arr[j++];
         }
-        else{
-            output[index] = arr[j];
-            j++, index++ ;
-        }
-    }
-    while(i<=mid){
-        output[index] = arr[i];
-        index++ ,  i++;
     }
 
-    while(j<=ei){
-        output[index] == arr[j];
-        j++, index++;
+    while(i<=mid){
+        temp[k++] = arr[i++];
     }
     
-    for(int k=si;k<=ei;k++){
-        arr[k] = output[l];
-        l++;
+    while(j<=ei){
+        temp[k++] = arr[j++];
+    }
+
+    for(int t=0;t<k;t++){
+        arr[si++] = temp[t];
     }
 }
-void mergeSort(int arr[],int si,int ei){
-    if(si>=ei){
+void mergeSort(int arr[], int si,int ei){
+    if(si >= ei){
         return;
     }
-    int mid = (si + ei)/2;
+
+    int mid = (si+ei)/2;
     mergeSort(arr,si,mid);
     mergeSort(arr,mid+1,ei);
-    mergeTwoSortedArrays(arr,si,ei);
+
+    merge(arr,si,mid,ei);
 }
 int main(){
     int n;
@@ -50,5 +46,4 @@ int main(){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
-    
 }
